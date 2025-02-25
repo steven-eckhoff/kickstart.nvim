78ab3d41d2f91edfc9dc9cda9caddc2ac9244720
Steven Eckhoff <steven.eckhoff.opensource@gmail.com>
2025-02-25 15:44:45 -0600

    enable gopls

--- init.lua
+++ init.lua
@@ -215,6 +215,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+-- Gopls
+-- NOTE: This is required fist `git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig`
+require('lspconfig').gopls.setup {}
+
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -660,7 +664,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
